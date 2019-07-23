56a7160086ba2b7104d17c13ad7bec64ff95e752
rickang <rickang@qq.com>
2019-07-23 15:50:35 +0800

    Add

--- src/main/java/com/iworkcloud/controller/IdeaController.java
+++ src/main/java/com/iworkcloud/controller/IdeaController.java
@@ -1,7 +1,28 @@
 package com.iworkcloud.controller;
 
+import com.iworkcloud.pojo.Idea;
+import com.iworkcloud.serviceImp.IdeaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+import java.sql.Timestamp;
 
 @Controller
 public class IdeaController {
+
+    @Autowired
+    private IdeaService ideaService;
+
+
+    @RequestMapping("addIdea")
+    public String addIdea(String title, String content, HttpSession session){
+
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        Idea idea = new Idea(session.getAttribute("staff").toString(),title,timestamp,content);
+
+        ideaService.addIdea(idea);
+        return "index";
+    }
 }
